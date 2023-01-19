4c84bcfcc8aaed63dcc61082e89006b2492aa5d8
mehmetgencv <mehmetgencv@gmail.com>
2023-01-20 00:03:42 +0300

    add template engine

--- app.js
+++ app.js
@@ -3,21 +3,25 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const hostname = "127.0.0.1";
+const enginer = require("express-handlebars");
 
 app.use(express.static("public"));
+
+app.engine("handlebars", enginer.engine());
+app.set("view engine", "handlebars");
 
 // "/" ifadesi yonlendirmenin nereye gidecegini belirtiyor
 // , den sonraki kisim callbeck fonksiyonu
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "site/index.html"));
+  res.render("site/index");
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "site/about.html"));
+  res.render("site/about");
 });
 
 app.get("/blog", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "site/blog.html"));
+  res.render("site/blog");
 });
 
 app.listen(port, hostname, () => {
