a98d330b6af6997a883d44fd34d1a9f46f199e16
mehmetgencv <mehmetgencv@gmail.com>
2023-01-21 18:08:47 +0300

    connection to mongodb

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const app = express();
 const port = 3000;
 const hostname = "127.0.0.1";
 const enginer = require("express-handlebars");
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/nodeblog_db')
+  .then(() => console.log('Connected!'));
 
 app.use(express.static("public"));
 
